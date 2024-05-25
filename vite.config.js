0c9d851e0ed0ff41a0109c3eb1485dfc9ca66f43
Prem Raj <premraj299341@gmail.com>
2024-05-25 14:05:50 +0530

    fix backend intigration

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://emergi-net-web-services-6bzifphlj-prem-rajs-projects.vercel.app',
-        secure: false,
-      },
-    },
-  },
   plugins: [react()],
 })
